769ef4a5dea2933006c388deb3303c694a2289f1
Anusri Pampari <pampari2@illinois.edu>
2018-08-14 23:18:26 -0500

    main script to run the code

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ from subprocess import check_call
 import sys
 
 
-## set the file paths in all the files called below##
+## set the file paths in all the files run below##
 PYTHON = sys.executable
 
 cmd = "{python} generation/i2b2_medications/medication-answers.py".format(python=PYTHON)
 print(cmd)
 check_call(cmd, shell=True)
+
+cmd = "{python} generation/i2b2_relations/relations-answers.py".format(python=PYTHON)
+print(cmd)
+check_call(cmd, shell=True)
+
+cmd = "{python} generation/i2b2_heart_disease_risk/risk-answers.py".format(python=PYTHON)
+print(cmd)
+check_call(cmd, shell=True)
+
+cmd = "{python} generation/i2b2_smoking/smoking-answers.py".format(python=PYTHON)
+print(cmd)
+check_call(cmd, shell=True)
+
+cmd = "{python} generation/i2b2_obesity/obesity-answers.py".format(python=PYTHON)
+print(cmd)
+check_call(cmd, shell=True)
+
 
 cmd = "{python} generation/combine_data/combine_answers.py".format(python=PYTHON)
 print(cmd)
